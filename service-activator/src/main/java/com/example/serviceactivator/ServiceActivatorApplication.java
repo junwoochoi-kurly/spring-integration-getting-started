a71075ee49bcadc1ff81885b6f726201df495942
junwoochoi-kurly <junwoo.choi@kurlycorp.com>
2024-07-26 17:08:09 +0900

    feat : project init

---
+++ service-activator/src/main/java/com/example/serviceactivator/ServiceActivatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceactivator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceActivatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceActivatorApplication.class, args);
+    }
+
+}
---
+++ service-activator/src/test/java/com/example/serviceactivator/ServiceActivatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceactivator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceActivatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
